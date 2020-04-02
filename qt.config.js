0fadf97bb014b940e1e039898800b969016a42a9
谢擎天 <xieqingtian@xieqingtiandeMacBook-Pro.local>
2020-04-03 03:44:06 +0800

    000

--- qt.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    sassResources: ['./src/sass/vars.scss', './src/sass/mixins.scss']
-};
---
+++ qt.config.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line spaced-comment
+/// <reference path="../qingtian-cli/typings/global.d.ts" />
+
+const projectConfig: NodeJS.Global['projectConfig'] = {
+    sassResources: ['src/sass/vars.scss', 'src/sass/mixins.scss'],
+};
+
+module.exports = projectConfig;
