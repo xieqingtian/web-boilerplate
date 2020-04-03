6806d41b1a8fbfcf14e21748730b5640a6a283be
xieqingtian <1255699673@qq.com>
2020-04-03 12:46:37 +0800

    888

--- qt.config.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line spaced-comment
-/// <reference path="/Users/xieqingtian/frontend/qingtian-cli/typings/global.d.ts" />
-
-const projectConfig: NodeJS.Global['projectConfig'] = {
-    sassResources: ['src/sass/vars.scss', 'src/sass/mixins.scss'],
-};
-
-module.exports = projectConfig;
